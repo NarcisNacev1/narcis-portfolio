dec646244d7d6e4ac940ba964de3bfa8213502f6
Narcis <nnaarrcciissnnaacceevv@gmail.com>
2025-08-25 08:03:41 +0200

    slight change and cursor still need to modify

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,20 @@ import Skills from './sections/Skills.tsx';
 import Projects from './sections/Projects.tsx';
 import Footer from './components/Footer.tsx';
 import VantaBackground from './styles/background/VantaBackground.tsx';
+import AnimatedCursor from 'react-animated-cursor';
 
 function App () {
 
     return (
         <>
+            <AnimatedCursor
+                innerSize={8}
+                outerSize={35}
+                color="255, 0, 150"
+                outerAlpha={0.4}
+                innerScale={0.7}
+                outerScale={2}
+            />
             <VantaBackground />
             <Navbar />
             <Intro/>
--- src/styles/components/Navbar.ts
+++ src/styles/components/Navbar.ts
@@ -1,10 +1,11 @@
 export const navLinks = {
     cursor: 'pointer',
     transition: 'color 0.5s ease-in-out',
+    fontFamily: "'Ariel'",
     _active: {
         color: '#FF00CC',
     },
     _hover: {
         color: '#FF00CC',
     },
-}
+};
